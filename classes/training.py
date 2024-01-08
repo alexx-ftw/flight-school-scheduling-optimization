0aad381202e9ee8606a99cf50af7754e1f8fa047
Alexx <eroszansan@gmail.com>
2024-01-08 00:36:01 +0000

    Add aircraft preferences and requirements to Training class

--- classes/training.py
+++ classes/training.py
@@ -24,16 +24,23 @@ class Training(object):
         self.lecture = lecture
 
         self.order = 0.0
+        self.preferred_aircraft = None
+        self.required_aircraft = None
         if "PPL" in self.program["name"] or "LAPL" in self.program["name"]:
             self.order = 1
+            self.preferred_aircraft = "2008"
         elif "TIME BUILDING" in self.program["name"]:
             self.order = 2
+            self.preferred_aircraft = "150"
         elif "IR" in self.program["name"] and "BASIC" in self.program["name"]:
             self.order = 3
+            self.required_aircraft = "2008"
         elif "NIGHT" in self.program["name"]:
             self.order = 4
+            self.required_aircraft = "2008"
         elif "MEP" in self.program["name"] and "Initial" in self.program["name"]:
             self.order = 5
+            self.required_aircraft = "2006"
         elif "IR-MEPL" in self.program["name"] and "Skill" not in self.name:
             self.order = 6
         elif "CPL" in self.program["name"] and "Skill" not in self.name:
@@ -44,6 +51,7 @@ class Training(object):
             self.order = 99
         elif "FI" in self.program["name"]:
             self.order = 100
+            self.preferred_aircraft = "2008"
 
         self.air_time_minutes = (
             lecture["vfrDualMinutes"]
